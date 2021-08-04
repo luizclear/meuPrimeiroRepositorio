cd66ec02847d179dd7703cec187efaa92361e4cb
luizclear <clemente10claro@gmail.com>
2021-08-04 20:50:23 -0300

    meu primeiro commit

---
+++ meuProjeto/meuProjeto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meuProjeto
+//
+//  Created by Luiz Felipe Clemente Claro on 04/08/21.
+//  Copyright © 2021 Luiz Felipe Clemente Claro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ projetoNovo/projetoNovo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projetoNovo
+//
+//  Created by Luiz Felipe Clemente Claro on 04/08/21.
+//  Copyright © 2021 Luiz Felipe Clemente Claro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
